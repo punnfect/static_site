1a03d9e167e33be488ecf8cd4f696152eb3f68a2
punnfect <sp.kool@yahoo.com>
2024-05-13 21:38:26 -0500

    finished html node, added textnode -> html(leafnode) converter

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,21 +6,75 @@ class HTMLNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError('Not implemented')
     
     #space before string is intentional idk why
     def props_to_html(self):
+        if self.props is None:
+            return ""
         edited_str = ''
         for key, value in self.props.items():
             edited_str += f' {key}="{value}"'
         return edited_str
     
+    # don't need
+    # def __eq__(self, other):
+    #     if (self.tag == other.tag and
+    #     self.value == other.value and
+    #     self.children == other.children and
+    #     self.props == other.props):
+    #         return True
+    #     else:
+    #         return False
+    
     def __repr__(self):
         return f"HTMLNode\nself.tag = {self.tag},\nself.value = {self.value},\nself.children = {self.children},\nself.props = {self.props}"
     
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"\nLeafNode\nself.tag = {self.tag},\nself.value = {self.value},\nself.props = {self.props}"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("No tag provided")
+        if self.children == None:
+            raise ValueError("No children provided")
+        html_str = ""
+        for i in range(len(self.children)):
+            html_str += f"{self.children[i].to_html()}"
+        return f"<{self.tag}{self.props_to_html()}>{html_str}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode\nself.tag = {self.tag},\nself.children =\n{self.children}\nself.props = {self.props}"
+
 def main():
-    sam = HTMLNode('1', '2', '3', {"href": "https://www.google.com", "target": "_blank"})
-    print(sam.props_to_html())
-    print(sam.__repr__())
+    node = ParentNode(
+    "p",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ],
+    {'href': 'https://www.google.com', 'target': '_blank'}
+)
+    print(node.to_html())
+    # print(node.__repr__())
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,8 +1,38 @@
+from logging import raiseExceptions
+from htmlnode import LeafNode, ParentNode, HTMLNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
+
+    def text_node_to_html_node(text_node):
+        match text_node.text_type:
+            case "text":
+                return LeafNode(None, text_node.text)
+            case "bold":
+                return LeafNode("b", text_node.text)
+            case "italic":
+                return LeafNode("i", text_node.text)
+            case "code":
+                return LeafNode("code", text_node.text)
+            case "link":
+                return LeafNode("a", text_node.text, {"href": text_node.url})
+            case "image":
+                return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+            case _:
+                raise Exception(f"{text_node.text_type} is not a valid text type")
+            
+        # def __init__(self, tag, value, props=None):
+        # super().__init__(tag, value, None, props)
 
     def __eq__(self, other):
         if (self.text == other.text and
