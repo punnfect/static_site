c9a85fd21dfdcc232fd5703b3bec7a5af3434fd8
punnfect <sp.kool@yahoo.com>
2024-05-21 01:49:57 -0500

    added recursive folder copy from ./static to ./public

---
+++ src/copystatic.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+# IMPORTANT file removal and addition will depend on where you call the function 
+# from static_site ./main.sh -- works with current version
+# or from src python main.py -- requires different path
+
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
+# def main():
+#     listdir()
+
+# main()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
+import os
 from textnode import TextNode
+import shutil
+from copystatic import copy_files_recursive
 
-# def main():
-#     sam = TextNode('hi', 'font_type', '/f/s/f/d')
-#     dog = TextNode('hi', 'font_type')
-#     print(sam.__eq__(dog))
-#     print(dog.__repr__())
 
-# main()
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+main()
